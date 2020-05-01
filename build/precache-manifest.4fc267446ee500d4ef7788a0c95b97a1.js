657fa1de847189e8df195a703865db5dd73215cb
trulli90 <trulli90@gmail.com>
2020-05-01 22:01:49 +0200

    adding robofriends files4

---
+++ build/precache-manifest.4fc267446ee500d4ef7788a0c95b97a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2afe3f7d3601481a04ea6636834f1ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "809a58d051fdc27ab303",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "a1be9d41b86c3d70246f",
+    "url": "/static/css/main.47feb694.chunk.css"
+  },
+  {
+    "revision": "809a58d051fdc27ab303",
+    "url": "/static/js/2.5773fc32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5773fc32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1be9d41b86c3d70246f",
+    "url": "/static/js/main.59744de7.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
